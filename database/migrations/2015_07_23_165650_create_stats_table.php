db2c31047820c0cc898c70939a6344d6aeec7662
Jamie Mansfield <malgm@lapis.blue>
2015-07-23 18:09:44 +0100

    Stats table migration

--- database/migrations/2015_07_23_165650_create_stats_table.php
+++ database/migrations/2015_07_23_165650_create_stats_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateStatsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,10 +12,10 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token')->index();
-            $table->timestamp('created_at');
+        Schema::create('stats', function ($t) {
+            $t->increments('id');
+            $t->string('key', 100);
+            $t->integer('value');
         });
     }
 
@@ -26,6 +26,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('password_resets');
+        Schema::drop('stats');
     }
 }
