da0c0630c6695e392c4636966ece2557dca22a4a
Jamie Mansfield <malgm@lapis.blue>
2015-07-23 22:35:01 +0100

    / route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return Response::make(
+        json_encode(array(
+            "error" => false,
+            "code" => 200,
+            "message" => null,
+            "data" => null
+        ), JSON_PRETTY_PRINT)
+    )->header('Content-Type', "application/json");
 });
+
+
