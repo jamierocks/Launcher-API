ee1fe747ea9450815d06647a35f09837362685f1
Jamie Mansfield <malgm@lapis.blue>
2015-07-24 08:22:44 +0100

    Display some data on the / route

---
+++ app/Http/Controllers/QixController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+
+class QixController extends Controller
+{
+
+    /**
+     * Returns the current systems uptime in seconds.
+     *
+     * Thanks to RyanTheAllmighty for this method
+     *
+     * @return int
+     */
+    public static function getSystemUptime() {
+        $fh = fopen('/proc/uptime', 'r');
+        $line = fgets($fh);
+        $uptime = (int)explode('.', $line)[0];
+        fclose($fh);
+        return $uptime;
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,7 +6,36 @@ Route::get('/', function () {
             "error" => false,
             "code" => 200,
             "message" => null,
-            "data" => null
+            "data" => array (
+                "load" => sys_getloadavg()[0],
+                "memory_use" => memory_get_usage(),
+                "uptime" => 999 // use QixController::getSystemUptime() on production
+            )
         ), JSON_PRETTY_PRINT)
     )->header('Content-Type', "application/json");
 });
+
+// V1
+/////
+
+Route::get('/v1/stats/downloads/', function () {
+
+});
+
+Route::get('/v1/stats/downloads/all/', function () {
+
+});
+
+Route::get('/v1/stats/downloads/exe/', function () {
+
+});
+
+Route::get('/v1/stats/downloads/zip/', function () {
+
+});
+
+Route::get('/v1/stats/downloads/jar/', function () {
+
+});
+
+
