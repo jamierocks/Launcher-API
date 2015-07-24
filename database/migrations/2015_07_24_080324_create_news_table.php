c339342da004b1f29f172cd234f20509c3f50efb
Jamie Mansfield <malgm@lapis.blue>
2015-07-24 09:25:37 +0100

    Add news table migration

---
+++ database/migrations/2015_07_24_080324_create_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function ($t) {
+            $t->increments('id');
+            $t->string('title');
+            $t->integer('comments');
+            $t->string('link');
+            $t->timestamp('published_at');
+            $t->text('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('news');
+    }
+}
